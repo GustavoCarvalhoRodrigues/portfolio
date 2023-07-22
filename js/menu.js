c502fcd9381ace53e00207abe1d2ed09039ce4b6
Gustavo Carvalho Rodrigues <90417550+GustavoCarvalhoRodrigues@users.noreply.github.com>
2023-07-22 16:35:54 -0300

    Uptade

--- js/menu.js
+++ js/menu.js
@@ -5,3 +5,11 @@ function menu() {
         itens.style.display = 'block'
     }
 }
+
+function tamanho() {
+    if (window.innerWidth >= 768) {
+        itens.style.display = 'flex'
+    } else {
+        itens.style.display = 'none'
+    }
+}
