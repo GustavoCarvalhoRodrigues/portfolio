72aac36eb1a28a35484ba6341be65e09f4d5a786
Gustavo Carvalho Rodrigues <90417550+GustavoCarvalhoRodrigues@users.noreply.github.com>
2023-08-08 16:07:17 -0300

    Arrumando responsivo

--- js/projetos.js
+++ js/projetos.js
@@ -1,12 +1,13 @@
 function conteudo() {
-    var cont = document.querySelector('#projetos')
+    var cont = document.getElementById('projetos')
     var cont2 = document.querySelector('.btp')
-    if (projetos.style.display == 'block') {
-        projetos.style.display = 'none'
+    if (cont.style.display == 'block') {
+         cont.style.display = 'none'
         cont2.innerHTML = 'Leia mais'
     } else {
-        projetos.style.display = 'block'
-        cont2.innerHTML = 'Leia menos'
-    }
+        cont.style.display = 'block'
+        cont2.style.display = 'none'
+    } 
 }
+
 
